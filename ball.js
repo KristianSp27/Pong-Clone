8c46ebfa49b9329027d27eb5beea2b081dfadc49
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-15 21:16:50 +0200

    Update

--- ball.js
+++ ball.js
@@ -20,11 +20,11 @@ set y(value) {
   this.ballElem.style.setProperty("--y", value)
 }
 
-resizeTo() {
+reset() {
   this.x = 50;
   this.y = 50;
   this.direction = {x: .75, y: .5};
-  while(true) {
+  while(this.direction.x <= .2 || this.direction.x >= .9) {
     const heading = randomNumberBetween(0, 2 * Math.PI);
     this.direction = { x: Math.cos(heading), y: Math.sin(heading)};
   }
