0fc37076703bb3737484d2b2d5c149e1e1461714
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-15 21:21:35 +0200

    Update

--- ball.js
+++ ball.js
@@ -1,3 +1,5 @@
+const INITIAL_VELOCITY = .025;
+
 export default class Ball {
   constructor(ballElem) {
     this.ballElem = ballElem;
@@ -28,9 +30,14 @@ reset() {
     const heading = randomNumberBetween(0, 2 * Math.PI);
     this.direction = { x: Math.cos(heading), y: Math.sin(heading)};
   }
+  this.velocity = INITIAL_VELOCITY;
 }
 
 update(delta) {
 this.x = 5;
 this.y = 15;
+}
+
+function randomNumberBetween(min, max) {
+return Math.random() * (max - min) + min;
 }
