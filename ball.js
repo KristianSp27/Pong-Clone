d5900bc16c5c8cb211eff0e0df74649d44259a1d
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-15 21:18:01 +0200

    Update

--- ball.js
+++ ball.js
@@ -24,7 +24,7 @@ reset() {
   this.x = 50;
   this.y = 50;
   this.direction = {x: .75, y: .5};
-  while(this.direction.x <= .2 || this.direction.x >= .9) {
+  while(Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
     const heading = randomNumberBetween(0, 2 * Math.PI);
     this.direction = { x: Math.cos(heading), y: Math.sin(heading)};
   }
