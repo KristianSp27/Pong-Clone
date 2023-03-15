11fcd3ebd5946beae5c80127c9d48f622814b7b3
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-15 21:14:52 +0200

    Update

--- ball.js
+++ ball.js
@@ -5,9 +5,32 @@ export default class Ball {
 }
 
 get x() {
-  return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+  return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+}
+
+set x(value) {
+  this.ballElem.style.setProperty("--x", value)
+}
+
+get y() {
+  return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+}
+
+set y(value) {
+  this.ballElem.style.setProperty("--y", value)
+}
+
+resizeTo() {
+  this.x = 50;
+  this.y = 50;
+  this.direction = {x: .75, y: .5};
+  while(true) {
+    const heading = randomNumberBetween(0, 2 * Math.PI);
+    this.direction = { x: Math.cos(heading), y: Math.sin(heading)};
+  }
 }
 
 update(delta) {
-
+this.x = 5;
+this.y = 15;
 }
