5e1cc06419d3abea4341d73a21778c5600a635fa
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-16 20:36:41 +0200

    Update

--- Paddle.js
+++ Paddle.js
@@ -2,4 +2,12 @@ export default class Paddle {
   constructor(paddleElem) {
     this.paddleElem = paddleElem;
   }
+
+  get position() {
+    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--position"));
+  }
+
+  set position(value) {
+    this.paddleElem.style.setProperty("--position", value);
+  }
 }
