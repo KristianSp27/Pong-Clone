86a97c18e94daf0791d8bdb30a8ae2a5ed40a4b2
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-16 20:27:10 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import Ball from "./ball.js";
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
 function update(time) {
