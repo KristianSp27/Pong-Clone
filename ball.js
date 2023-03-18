18d17e772e638a3201331d6d319d492a96bed675
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-18 18:04:30 +0200

    Update

--- ball.js
+++ ball.js
@@ -48,11 +48,15 @@ if (rect.bottom >= window.innerHeight || rect.top <= 0) {
   this.direction.y *= 1;
 }
 
-if (rect.right >= window.innerWidth || rect.left <= 0) {
+if (paddleRects.some(r => isCollision(r, rect))) {
   this.direction.x *= -1;
 }
 }
 
 function randomNumberBetween(min, max) {
 return Math.random() * (max - min) + min;
+}
+
+function isCollision(rect1, rect2) {
+  return rect1 <= rect2.right && rect1.right >= rect2.left && rect1.top <= rect2.bottom && rect1.bottom >= rect2.top
 }
