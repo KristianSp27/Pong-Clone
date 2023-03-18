3eb2469715fed59f4f31f012972e5c075b1cea3e
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-18 17:56:59 +0200

    Update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import Paddle from "./Paddle.js";
 const ball = new Ball(document.getElementById("ball"));
 const playerPaddle = new Paddle(document.getElementById("player-paddle"));
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+const playerScoreElem = document.getElementById("player-score");
+const computerScoreElem = document.getElementById("computer-score");
 
 let lastTime;
 function update(time) {
@@ -11,10 +13,23 @@ function update(time) {
     const delta = time - lastTime;
     ball.update(delta);
     computerPaddle.update(delta, ball.y);
+
+    if (isLose()) handleLose();
   }
 
   lastTime = time;
   window.requestAnimationFrame(update);
+}
+
+function isLose() {
+  const rect = ball.rect();
+  return rect.right >= window.innerWidth || rect.left <= 0;
+}
+
+function handleLose() {
+  const rect = ball.rect();
+  if (rect.right >= window.innerWidth || rect.left <= 0) ball.reset();
+  computerPaddle.reset();
 }
 
 document.addEventListener("mousemove", (e) => {
